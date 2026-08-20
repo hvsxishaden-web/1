23650ee7dfa32f69c47616c21730b3b27dfb71dc
hvsxishaden-web <hvsxishaden@gmail.com>
2026-08-20 19:30:33 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from './App.tsx';
 import { FavoritesProvider } from './context/FavoritesContext.tsx';
 import './index.css';
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <FavoritesProvider>
